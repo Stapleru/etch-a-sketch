cd108d7ac02ad4ee9058f1c47686b8b32a82b30b
Stapleru <andrejutinka@mail.ru>
2023-08-22 13:13:26 +0300

    Add color cell function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let isMouseDown = false;
+document.body.onmousedown = () => {isMouseDown = true};
+document.body.onmouseup = () => {isMouseDown = false};
+
 function createGrid(size) {
 
     const grid = document.querySelector('.container')
@@ -5,10 +9,19 @@ function createGrid(size) {
     for(let i = 0; i < size*size; i++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.addEventListener('mouseover', colorCell);
+        cell.addEventListener('mousedown', colorCell);
         grid.appendChild(cell);
     }
     grid.style.cssText = `grid-template-columns: repeat(${size}, 1fr); 
                           grid-template-rows: repeat(${size}, 1fr)`;
 }
 
-createGrid(16);
+function colorCell(e){
+    if(e.type == 'mouseover' && isMouseDown) {
+        this.style.backgroundColor = 'black';
+        this.style.border = 'none';
+    }
+}
+
+createGrid(100);
